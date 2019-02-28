ebb7f393c8dbf7bd056f5931673cd2204109b78c
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-02-28 16:26:35 -0600

    added tests

--- lambda-classes.js
+++ lambda-classes.js
@@ -65,3 +65,46 @@ class ProjectManager extends Instructor {
         console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
+
+const christian_ipanaque = new Student({
+    name: "Christian Ipanaque",
+    age: 32,
+    gender: "Male",
+    location: "Seattle, WA",
+    previousBackground: "Server Hardware Technician",
+    className: "WEB18",
+    favSubjects: ["Computer Science", "Redux", "Algorithms", "Data Structures", "Computer Architecture"],
+});
+
+christian_ipanaque.speak();
+christian_ipanaque.listsSubjects();
+christian_ipanaque.PRAssignment('Javascript IV');
+christian_ipanaque.sprintChallenge('Intro to Javascript');
+
+const liz_B = new Instructor({
+    name: "Lizzy B",
+    age: 27,
+    gender: "Female",
+    location: "Portland, OR",
+    previousBackground: "Biology/Health Science grad",
+    className: "WEB12",
+    favSubjects: ["CSS", "React", "Ternaries", "SQL", "Node"],
+});
+
+liz_B.speak();
+liz_B.demo('Javascript');
+liz_B.grade(christian_ipanaque, 'Javascript IV');
+
+const joshk = new ProjectManager({
+    name: "Josh Kersting",
+    age: 27,
+    gender: "Male",
+    location: "Muskegon, Mi",
+    previousBackground: "Data Analyst",
+    className: "WEB18",
+    favSubjects: ["Computer Science", "Algorithms", "Data Structures", "Computer Architecture", "Photography", "Cooking"],
+});
+
+joshk.speak();
+joshk.standUp('Web18');
+joshk.debugsCode(christian_ipanaque, 'Javascript IV');

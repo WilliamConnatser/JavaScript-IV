b1d5ea9342be1ba5ebadf5d005b276e0b2f1e82c
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-02-28 15:55:20 -0600

    finished assignment

--- lambda-classes.js
+++ lambda-classes.js
@@ -1,1 +1,67 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(params) {
+        this.name = params.name;
+        this.age = params.age;
+        this.location = params.location;
+        this.gender = params.gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    }
+}
+
+class Instructor extends Person {
+    constructor(params) {
+        super(params)
+        this.specialty = params.specialty;
+        this.favLanguage = params.favLanguage;
+        this.catchPhrase = params.catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person {
+    constructor(params) {
+        super(params);
+        this.previousBackground = params.previousBackground;
+        this.className = params.className;
+        this.favSubjects = params.favSubjects;
+    }
+
+    listsSubjects() {
+        this.favSubjects.forEach(subj => console.log(subj));
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun the sprint challenge on ${subject}`);
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(params) {
+        super(params);
+        this.gradClassName = params.gradClassName;
+        this.favInstructor = params.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
--- prototype-refactor.js
+++ prototype-refactor.js
@@ -8,146 +8,125 @@ Prototype Refactor
 
 */
 
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
-
-function GameObject(args) {
-    this.createdAt = args.createdAt;
-    this.name = args.name;
-    this.dimensions = args.dimensions;
+class GameObject {
+  constructor(params) {
+    this.createdAt = params.createdAt;
+    this.name = params.name;
+    this.dimensions = params.dimensions;
   }
-  
-  GameObject.prototype.destroy = function () {
+
+  destroy() {
     return `${this.name} was removed from the game.`;
   }
-  
-  /*
-    === CharacterStats ===
-    * healthPoints
-    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-    * should inherit destroy() from GameObject's prototype
-  */
-  
-  function CharacterStats(args) {
-    this.healthPoints = args.healthPoints;
-    GameObject.call(this, args); //Gain access to parent's arguments
+}
+
+
+class CharacterStats extends GameObject {
+  constructor(params) {
+    super(params);
+    this.healthPoints = params.healthPoints;
   }
-  
-  CharacterStats.prototype = Object.create(GameObject.prototype); //Gain access to parent's prototypes
-  CharacterStats.prototype.takeDamage = function () {
+
+  takeDamage() {
     return `${this.name} took damage.`;
   }
-  
-  /*
-    === Humanoid (Having an appearance or character resembling that of a human.) ===
-    * team
-    * weapons
-    * language
-    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    * should inherit destroy() from GameObject through CharacterStats
-    * should inherit takeDamage() from CharacterStats
-  */
-  
-  
-  function Humanoid(args) {
-    this.team = args.team;
-    this.weapons = args.weapons;
-    this.language = args.language;
-    CharacterStats.call(this, args);
+}
+
+class Humanoid extends CharacterStats {
+  constructor(params) {
+    super(params)
+    this.team = params.team;
+    this.weapons = params.weapons;
+    this.language = params.language;
   }
-  
-  Humanoid.prototype = Object.create(CharacterStats.prototype);
-  Humanoid.prototype.greet = function () {
+
+  greet() {
     return `${this.name} offers a greeting in ${this.language}.`;
   }
-  
-  /*
-   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-   * Instances of CharacterStats should have all of the same properties as GameObject.
-   */
-  
-  // Test you work by un-commenting these 3 objects and the list of console logs below:
-  
-  
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-  
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-  
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  
-  
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
-  
-  function Villain(args) {
-    Humanoid.call(this, args);
+}
+
+/*
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+// console.log(mage.createdAt); // Today's date
+// console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+// console.log(swordsman.healthPoints); // 15
+// console.log(mage.name); // Bruce
+// console.log(swordsman.team); // The Round Table
+// console.log(mage.weapons); // Staff of Shamalama
+// console.log(archer.language); // Elvish
+// console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+// console.log(mage.takeDamage()); // Bruce took damage.
+// console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+class Villain extends Humanoid {
+  constructor(params) {
+    super(params);
   }
-  
-  Villain.prototype = Object.create(Humanoid.prototype);
-  Villain.prototype.nuclearBomb = function (target) {
+
+  nuclearBomb(target) {
     if (target.healthPoints <= 0) {
       console.log(`${target.name} is already dead. :\\`);
       return;
@@ -159,17 +138,18 @@ function GameObject(args) {
       console.log(target.destroy())
       target.destroy();
     }
-  
+
     return target;
   }
-  
-  function Hero(args) {
-    Humanoid.call(this, args);
+}
+
+class Hero extends Humanoid {
+  constructor(params) {
+    super(params);
   }
-  
-  Hero.prototype = Object.create(Humanoid.prototype);
-  Hero.prototype.guidedMissiles = function (target) {
-  
+
+  guidedMissiles(target) {
+
     if (target.healthPoints <= 0) {
       console.log(`${target.name} is already dead. :\\`);
       return;
@@ -177,93 +157,94 @@ function GameObject(args) {
     const attackDamage = 5;
     console.log(`${target.name} was attacked by ${this.name} for ${attackDamage} health.`);
     target.healthPoints -= attackDamage;
-  
+
     if (target.healthPoints <= 0) {
       console.log(target.destroy())
       target.destroy();
     }
-  
+
     return target;
   }
-  
-  const american = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2
-    },
-    healthPoints: 10,
-    name: 'America!',
-    team: 'The West',
-    weapons: [
-      'Guided Missiles'
-    ],
-    language: 'English',
-  });
-  
-  
-  
-  const terrorist = new Villain({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4
-    },
-    healthPoints: 10,
-    name: 'Terrorist',
-    team: 'ISIS',
-    weapons: [
-      'Nuclear Bomb'
-    ],
-    language: 'Arabic',
-  });
-  
-  
-  
-  window.onload = function () {
-  
-    let heroInfo = document.querySelector('.hero-info');
-    Object.keys(american).forEach(key => {
-      heroInfo.textContent += `${key}: ${american[key]}
+}
+
+const american = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 10,
+  name: 'America!',
+  team: 'The West',
+  weapons: [
+    'Guided Missiles'
+  ],
+  language: 'English',
+});
+
+
+
+const terrorist = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: 'Terrorist',
+  team: 'ISIS',
+  weapons: [
+    'Nuclear Bomb'
+  ],
+  language: 'Arabic',
+});
+
+
+
+window.onload = function () {
+
+  let heroInfo = document.querySelector('.hero-info');
+  Object.keys(american).forEach(key => {
+    heroInfo.textContent += `${key}: ${american[key]}
       `;
-    });
-  
-    heroInfo.innerHTML = heroInfo.innerHTML.replace(/\n\r?/g, '<br/>');
-   
-  
-    let VillainInfo = document.querySelector('.villain-info');
-    Object.keys(terrorist).forEach(key => {
-        VillainInfo.textContent += `${key}: ${terrorist[key]}
+  });
+
+  heroInfo.innerHTML = heroInfo.innerHTML.replace(/\n\r?/g, '<br/>');
+
+
+  let VillainInfo = document.querySelector('.villain-info');
+  Object.keys(terrorist).forEach(key => {
+    VillainInfo.textContent += `${key}: ${terrorist[key]}
         `;
-    });
-  
-    VillainInfo.innerHTML = VillainInfo.innerHTML.replace(/\n\r?/g, '<br/>');
-  
-    let villainAttack = document.querySelector('.villain-attack');
-    let heroAttack = document.querySelector('.hero-attack');
-  
-    villainAttack.addEventListener("click", _ => {
-      terrorist.nuclearBomb(american);
-  
-      if(american.healthPoints <= 0) {
-        heroInfo.style.background = 'red';
-      }
-    });
-  
-    heroAttack.addEventListener("click", _ => {
-      american.guidedMissiles(terrorist);
-  
-      if(terrorist.healthPoints <= 0) {
-        VillainInfo.style.background = 'red';
-      } else {
-        VillainInfo.style.background = 'red';
-        setTimeout(
-          () => {
-            VillainInfo.style.background = 'white';
-          },50
-        )
-      }
-    });
-  }
+  });
+
+  VillainInfo.innerHTML = VillainInfo.innerHTML.replace(/\n\r?/g, '<br/>');
+
+  let villainAttack = document.querySelector('.villain-attack');
+  let heroAttack = document.querySelector('.hero-attack');
+
+  villainAttack.addEventListener("click", _ => {
+    terrorist.nuclearBomb(american);
+
+    if (american.healthPoints <= 0) {
+      heroInfo.style.background = 'red';
+    }
+  });
+
+  heroAttack.addEventListener("click", _ => {
+    american.guidedMissiles(terrorist);
+
+    if (terrorist.healthPoints <= 0) {
+      VillainInfo.style.background = 'red';
+    } else {
+      VillainInfo.style.background = 'red';
+      setTimeout(
+        () => {
+          VillainInfo.style.background = 'white';
+        }, 50
+      )
+    }
+  });
+}
